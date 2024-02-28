ed725e7c0e56f3938103bce26c58dbf1eda2585f
mahermahasiswa2002 <161567450+mahermahasiswa2002@users.noreply.github.com>
2024-02-28 14:56:38 +0700

    first

---
+++ sena.py
@@ -0,0 +1,14 @@
+# length = float(input("please type the length: \n "))
+# width = float(input("please type the width: \n"))
+# area = length * width 
+# print(f"the length is {str(length)} \nthe width is {str(width)} \nthe total area is {str(area)}")
+# cost = float(input("how much for 1 meter? \n"))
+
+# print(f"you should give the worker around: {str(cost * area)}")
+name = "Maher"
+age = "22"
+address = "sikabumi"
+hobby = "football"
+gender = "male"
+print(f"my name is, {name} \nmy age is, {age} \ni live in {address} \ni like to play {hobby} \ni am a {gender}")
+
